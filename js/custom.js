ecafb5720ad0ba22cd0cfea9d42f97cb307c60ae
KOKITERASAKI <terablog.0326@gmail.com>
2019-09-20 23:32:35 +0900

    050 done work section

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,23 @@
 /*=======================================
                   SERVICES
-=======================================*/ 
-$(function(){
+=======================================*/
+$(function () {
   // animate on scroll
   new WOW().init();
+
+});
+
+/*=======================================
+                  WORK
+=======================================*/
+$(function () {
+  $("#work").magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    // other options
+    gallery:{
+      enabled:true
+    }
+  });
 
 });
